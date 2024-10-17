6dc3b740b4df4b59474de939c0c1787b1c961c2c
Mita vaja <mitavaja3@gmail.com>
2024-10-17 14:52:43 +0530

    Delete TO DO app directory

--- TO DO app/db.js
+++
@@ -1,8 +0,0 @@
-
-const { default: mongoose } = require('mongoose')
-const mongose = require('mongoose')
-const dbConnect = async () => {
-    await mongoose.connect("mongodb+srv://mitavaja:node@cluster0.ay6rw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-}   
-console.log("connected to db");
-module.exports = dbConnect
