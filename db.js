ffdd8d9288148212a30708bcc1dea43704f1e203
Mita vaja <mitavaja3@gmail.com>
2024-10-21 10:22:21 +0530

    Delete db.js

--- db.js
+++
@@ -1,7 +0,0 @@
-const { default: mongoose } = require('mongoose')
-const mongose = require('mongoose')
-const dbConnect = async () => {
-   await mongoose.connect("mongodb+srv://mitavaja:node@cluster0.djwux.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-}
-console.log("connected to db");
-module.exports = dbConnect
