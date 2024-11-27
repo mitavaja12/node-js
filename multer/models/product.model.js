87f9fd304864c65eb981c35825fa98271db1aee0
Mita vaja <mitavaja3@gmail.com>
2024-11-27 19:57:45 +0530

    multer

---
+++ multer/models/product.model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  title: String,
+  img: String,
+  price: Number,
+  ratings: [{}],
+});
+
+
+const Product=mongoose.model("Product",productSchema);
+module.exports = Product;
---
+++ multer/routes/product.router.js
@@ -0,0 +1,18 @@
+const { Router } = require("express");
+const {
+  getProducts,
+  getProductById,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+} = require("../controllers/product.controller");
+const upload = require("../utils/imageupload");
+
+const productRoute = Router();
+productRoute.get("/", getProducts);
+productRoute.get("/:productId", getProductById);
+productRoute.post("/",upload.single("img"), createProduct);
+productRoute.delete("/:productId", deleteProduct);
+productRoute.patch("/:productId", updateProduct);
+
+module.exports = productRoute;
