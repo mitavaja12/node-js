0fb546732c25034bdc6bb9537e4d8b5398cd2ef9
Mita vaja <mitavaja3@gmail.com>
2025-01-07 14:30:24 +0530

    Delete Password Reset directory

--- Password Reset/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  await mongoose.connect("mongodb://localhost:27017/resetpassword");
-  console.log("MongoDB connected");
-};
-
-module.exports = connectDB;
