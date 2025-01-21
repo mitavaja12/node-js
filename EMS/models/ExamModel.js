3b6c2799a58f8c1837b4ed6f9799b66c9841daa5
Mita vaja <mitavaja3@gmail.com>
2025-01-21 11:41:54 +0530

    Delete EMS/models directory

--- EMS/models/ExamModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const examSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    questions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }]
-});
-
-module.exports = mongoose.model('Exam', examSchema);
--- EMS/models/QuestionModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const questionSchema = new mongoose.Schema({
-    question: { type: String, required: true },
-    options: String,
-    correctAnswer: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Question', questionSchema);
